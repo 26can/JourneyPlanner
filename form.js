9539ebf74de13ee915f17b1055b14f2b2ad0d55a
26can <nicolefortunecat@msn.cn>
2019-05-20 07:43:27 +0300

    1st api request

--- form.js
+++ form.js
@@ -1,44 +1,22 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Directions Service</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-    <div id="floating-panel">
-    <b>Start: </b>
-    <select id="start">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-      <option value="oklahoma city, ok">Oklahoma City</option>
-      <option value="amarillo, tx">Amarillo</option>
-      <option value="gallup, nm">Gallup, NM</option>
-      <option value="flagstaff, az">Flagstaff, AZ</option>
-      <option value="winona, az">Winona</option>
-      <option value="kingman, az">Kingman</option>
-      <option value="barstow, ca">Barstow</option>
-      <option value="san bernardino, ca">San Bernardino</option>
-      <option value="los angeles, ca">Los Angeles</option>
-    </select>
-    <b>End: </b>
-    <select id="end">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-      <option value="oklahoma city, ok">Oklahoma City</option>
-      <option value="amarillo, tx">Amarillo</option>
-      <option value="gallup, nm">Gallup, NM</option>
-      <option value="flagstaff, az">Flagstaff, AZ</option>
-      <option value="winona, az">Winona</option>
-      <option value="kingman, az">Kingman</option>
-      <option value="barstow, ca">Barstow</option>
-      <option value="san bernardino, ca">San Bernardino</option>
-      <option value="los angeles, ca">Los Angeles</option>
-    </select>
-    </div>
+const request = require('request');
 
-  </body>
-</html>
+var req = {
+  url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  method: 'POST',
+  headers: { "Content-Type": "application/json" },
+  body: { "query": `{
+    stop(id: \"HSL:1040129\") {
+      name
+      lat
+      lon
+      wheelchairBoarding
+    }
+  }` },
+  json: true
+};
+
+request(req, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(JSON.stringify(body, null, 4));
+  }
+});
